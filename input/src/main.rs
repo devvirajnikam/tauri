b54cc23877bb1d0846cd6518e76502a8bce37c24
dev-viraj <viraj.nikam@zoyo.in>
2026-07-11 10:10:12 +0530

    feat : practiced input

--- input/src/main.rs
+++ input/src/main.rs
@@ -1,11 +1,27 @@
 use std::io;
 
-fn simple_read_input()-> (){
+fn _simple_read_input()-> (){
     let mut command = String::new();
     io::stdin().read_line(&mut command).expect("can't read line");
     println!("Worked {}", command);
 }
 
+fn match_result()->(){
+    let mut command = String::new();
+    let result = io::stdin().read_line(&mut command);
+
+    match &result {
+        Ok(data) => {
+            println!("Success, {:#?}", data);
+            println!("The Input Was, {}", command);
+            println!("The Result was. {:#?}", result);
+        }
+        Err(e)=>{
+            println!("Failed, {:#?}", e)
+        }
+    }
+}
+
 fn main(){
-    simple_read_input();
+    match_result();
 }
